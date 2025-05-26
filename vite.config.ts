17cb175a3014e93ec15f8b8599a091d6f65b5998
Wsdtik <dr.bibka@gmail.com>
2025-05-26 23:08:00 +0300

    changed vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	build: {
-		rollupOptions: {
-			input: {
-				theme: './src/themes/bongo-theme.css'
-			}
-		}
+		cssCodeSplit: true,
 	}
 });
